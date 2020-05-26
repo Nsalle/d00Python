4fc1c1bef3e117753676fb9ed234ef06ee47ec83
nsalle <nicolas.salle@free.fr>
2020-05-26 10:53:04 +0200

    go d01

--- ex10/loading.py
+++ ex10/loading.py
@@ -1,9 +1,25 @@
+from time import sleep
+from time import time
+import subprocess
+
 listy = range(1000)
 ret = 0
 
 
 def ft_progress(lst):
-    
+    startime = time()
+    percent = 0
+    turn = 0
+    maxr = len(lst)
+    while percent < len(lst):
+        percent = turn / maxr * 100
+        subprocess.call("clear")
+        curtime = time() - startime
+        print("ETA: {:.2f}s\t".format(curtime), end='')
+        print("[{:3.0f}%] ".format(percent), end='')
+        print("{}/{}".format(turn, maxr))
+        yield turn
+        turn += 1
 
 
 for elem in ft_progress(listy):
